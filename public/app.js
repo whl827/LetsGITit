20d4e19c920d5ba685314a7008baee9458a3a32f
WHL\WoongHee <woonghel@usc.edu>
2017-10-15 01:43:24 -0700

    routing updated

--- public/app.js
+++ public/app.js
@@ -7,7 +7,16 @@ app.config(function($routeProvider) {
     .when("/profile", {
         templateUrl : "pages/profile.html"
     })
-    .when("/blue", {
-        templateUrl : "blue.htm"
+    .when("/login", {
+        templateUrl : "pages/login.html"
+    })
+    .when("/signup", {
+        templateUrl : "pages/signUp.html"
+    })
+    .when("/createPoll", {
+        templateUrl : "pages/createPoll.html"
+    })
+    .when("/createRating", {
+        templateUrl : "pages/createRating.html"
     });
 });
