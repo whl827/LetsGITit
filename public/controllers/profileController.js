49649e60b17d3836cfa7687524892cd013d4eb02
Robert Diersing <rdiersin@usc.edu>
2017-10-15 16:42:43 -0700

    imported npCookies

---
+++ public/controllers/profileController.js
@@ -0,0 +1,5 @@
+angular.module("KnowItAll").controller('ProfileCtrl', ['$scope', '$http', function($scope, $http) {
+
+	
+    
+}]);
---
+++ public/route.js
@@ -0,0 +1,21 @@
+angular.module("KnowItAll").config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl : "pages/home.html"
+    })
+    .when("/profile", {
+        templateUrl : "pages/profile.html"
+    })
+    .when("/login", {
+        templateUrl : "pages/login.html"
+    })
+    .when("/signup", {
+        templateUrl : "pages/signUp.html"
+    })
+    .when("/createPoll", {
+        templateUrl : "pages/createPoll.html"
+    })
+    .when("/createRating", {
+        templateUrl : "pages/createRating.html"
+    });
+});
