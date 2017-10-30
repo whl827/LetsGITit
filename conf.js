7189a94d0590c2e1e872a1192fd2adcbd77d19fe
Robert Diersing <rdiersin@usc.edu>
2017-10-30 15:16:53 -0700

    all tests work for search login rating and poll creation:

--- conf.js
+++ conf.js
@@ -3,3 +3,4 @@ exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: [ 'blackbox/searchBar.spec.js', 'blackbox/login.spec.js',
    'blackbox/createPoll.spec.js', 'blackbox/createRating.spec.js']
+}
